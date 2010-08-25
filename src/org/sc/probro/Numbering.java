2884f4a2d65ea9b5143a72d3abfefea9402ca5a3
tdanford <tdanford@301305c5-f827-0410-9252-dba5032f77be>
2010-08-25 21:23:37 +0000

    Added the new Numbering class.
    git-svn-id: http://svn.neurocommons.org/svn/trunk/broker@3634 301305c5-f827-0410-9252-dba5032f77be

---
+++ src/org/sc/probro/Numbering.java
@@ -0,0 +1,17 @@
+package org.sc.probro;
+
+public class Numbering<S> extends Pairing<S,Integer> {
+	
+	public static <T> Integer[] range(T[] ss) { 
+		Integer[] idx = new Integer[ss.length];
+		for(int i = 0; i < ss.length; i++) { idx[i] = i; }
+		return idx;
+	}
+
+	public Numbering(S... ss) { 
+		super(ss, range(ss));
+	} 
+	
+	public int number(S s) { return forward(s); }
+	public S value(int idx) { return backward(idx); }
+}
--- src/org/sc/probro/Pairing.java
+++ src/org/sc/probro/Pairing.java
@@ -29,18 +29,3 @@ public class Pairing<S,T> {
 	public int secondIndex(T t) { return tidx.get(s); }
 }
 
-class Numbering<S> extends Pairing<S,Integer> {
-	
-	public static <T> Integer[] range(T[] ss) { 
-		Integer[] idx = new Integer[ss.length];
-		for(int i = 0; i < ss.length; i++) { idx[i] = i; }
-		return idx;
-	}
-
-	public Numbering(S... ss) { 
-		super(ss, range(ss));
-	} 
-	
-	public int number(S s) { return forward(s); }
-	public S value(int idx) { return backward(idx); }
-}
