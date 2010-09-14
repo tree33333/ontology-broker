69010dfc1b8073301cc78e3e745d8ac468974f6d
tdanford <tdanford@301305c5-f827-0410-9252-dba5032f77be>
2010-09-14 17:41:54 +0000

    Added a set of server-availability tests for different paths.
    git-svn-id: http://svn.neurocommons.org/svn/trunk/broker@3739 301305c5-f827-0410-9252-dba5032f77be

--- src/org/sc/probro/tests/ServerTests.java
+++ src/org/sc/probro/tests/ServerTests.java
@@ -17,16 +17,45 @@ public class ServerTests {
 	
 	public static String server = "http://ashby.csail.mit.edu:8080";
 	
+	public static String[] paths = new String[] { 
+		"/ontologies",
+		"/users",
+		"/requests",
+		"/bulk-requests",
+	};
+	
+	public static String urlString(String suffix) { 
+		if(suffix.startsWith("/")) { 
+			return server + suffix;
+		} else { 
+			return server + "/" + suffix;
+		}
+	}
+	
 	@org.junit.Test
-	public void testServerAvailability() throws IOException { 
-		String urlstring = server + "/";
-		URL url = new URL(urlstring);
+	public void testServerAvailability() throws IOException {
+		
+		for(String path : paths) { 
+			int responseCode = http_ResponseCode("GET", path);
+			assertTrue(
+					String.format("GET %s returned code %d", 
+							urlString(path), responseCode),
+					responseCode==200);
+		}
+	}
+
+	public static int http_ResponseCode(String method, String path) throws IOException { 
+		URL url = new URL(urlString(path));
 		HttpURLConnection cxn = (HttpURLConnection) url.openConnection();
-		cxn.connect();
-		int responseCode = cxn.getResponseCode();
-		assertTrue(
-				String.format("Server %s returned response code %d", urlstring, responseCode),
-				responseCode == 200);
+		cxn.setRequestMethod(method);
+		try { 
+			cxn.connect();
+			int responseCode = cxn.getResponseCode();
+			cxn.getInputStream().close();
+			return responseCode;
+		} catch(Exception e) { 
+			return cxn.getResponseCode();
+		}
 	}
 	
 	public static String httpPUT_JSONObject(String urlstring, JSONObject postObj) throws IOException { 
