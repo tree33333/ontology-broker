b8acf75672236c60a0f080bbea09f6961ef3b4fc
tdanford <tdanford@301305c5-f827-0410-9252-dba5032f77be>
2010-09-15 18:31:13 +0000

    A set of tests for the JSON schemas in docs/json-schemas/
    git-svn-id: http://svn.neurocommons.org/svn/trunk/broker@3744 301305c5-f827-0410-9252-dba5032f77be

---
+++ src/org/sc/probro/tests/JSONTests.java
@@ -0,0 +1,119 @@
+package org.sc.probro.tests;
+
+import java.io.*;
+
+import static org.junit.Assert.*;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import tdanford.json.schema.JSONObjectType;
+import tdanford.json.schema.JSONType;
+import tdanford.json.schema.SchemaEnv;
+import tdanford.json.schema.SchemaException;
+
+import java.io.IOException;
+import java.util.*;
+
+public class JSONTests {
+	
+	@org.junit.Test 
+	public void testJSONSchemas() throws IOException {
+		BrokerSchemaEnv env = new BrokerSchemaEnv();
+		
+		testContains(env.getMetadataType(), new MetadataExample());
+		testContains(env.getRequestType(), new RequestExample());
+		testContains(env.getUserType(), new UserExample());
+		testContains(env.getOntologyType(), new OntologyExample());
+	}
+	
+	public void testContains(JSONType type, Object value) { 
+		assertTrue(type.explain(value), type.contains(value));
+	}
+}
+
+class BrokerSchemaEnv extends SchemaEnv { 
+	public BrokerSchemaEnv() { 
+		super(new File("docs/json-schemas/"));
+	}
+	
+	public JSONType getRequestType() { return lookupType("Request"); } 
+	public JSONType getMetadataType() { return lookupType("Metadata"); } 
+	public JSONType getUserType() { return lookupType("User"); }
+	public JSONType getOntologyType() { return lookupType("Ontology"); }
+	public JSONType getSearchResultType() { return lookupType("SearchResult"); }
+}
+
+class JSONFile extends JSONObject { 
+	public JSONFile(File f) throws IOException { 
+		super(new FileReader(f));
+	}
+}
+
+class UserExample extends JSONObject { 
+	public UserExample() { 
+		try {
+			put("user_id", 1);
+			put("user_name", "Test Name");
+		} catch (JSONException e) {
+			throw new IllegalStateException(e);
+		}
+	}
+}
+
+class OntologyExample extends JSONObject { 
+	public OntologyExample() { 
+		try {
+			put("ontology_id", 10);
+			put("name", "Test Name");
+		} catch (JSONException e) {
+			throw new IllegalStateException(e);
+		}
+	}
+}
+
+class RequestExample extends JSONObject {
+	
+	public RequestExample() { 
+		super();
+		try {
+			put("search_text", "foo");
+			put("context", "bar blah blah blah");
+			put("provenance", "http://example.com/blah");
+			put("date_submitted", "2010-09-01");
+			put("status", 200);
+			put("ontology_id", "grok");
+			put("provisional_id", "quux");
+			
+			put("created_by", 10);
+			
+			append("metadata", new MetadataExample(this, "akey", "bvalue"));
+			append("metadata", new MetadataExample(this, "ckey", "dvalue"));
+		} catch (JSONException e) {
+			throw new IllegalStateException(e);
+		}
+	}
+}
+
+class MetadataExample extends JSONObject {
+	
+	public MetadataExample() { 
+		super();
+		try {
+			put("metadata_key", "foo");
+			put("metadata_value", "bar");
+		} catch (JSONException e) {
+			throw new IllegalStateException(e);
+		}		
+	}
+	public MetadataExample(RequestExample req, String key, String value) { 
+		super();
+		try {
+			put("metadata_key", key);
+			put("metadata_value", value);
+		} catch (JSONException e) {
+			throw new IllegalStateException(e);
+		}
+	}
+}
+
