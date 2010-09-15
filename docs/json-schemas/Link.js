71204fb31c7b17a31726b6539515804ef2f5099b
tdanford <tdanford@301305c5-f827-0410-9252-dba5032f77be>
2010-09-15 19:04:49 +0000

    Expanded the description of the Metadata schema, and added the new Link schema.
    git-svn-id: http://svn.neurocommons.org/svn/trunk/broker@3752 301305c5-f827-0410-9252-dba5032f77be

---
+++ docs/json-schemas/Link.js
@@ -0,0 +1,12 @@
+
+{ 
+    name : "Link",
+    type : "object",
+    
+    properties : { 
+        href : { 
+            type : "string",
+            description : "A URL which identifies the entity to which this Link is a reference.",
+        },
+    },
+}
--- docs/json-schemas/Metadata.js
+++ docs/json-schemas/Metadata.js
@@ -1,9 +1,31 @@
 
 {
-   "name" : "Metadata",
-   "type" : "object",
-   "properties" : { 
-     "metadata_key" : "string",
-     "metadata_value" : "string"
+   name : "Metadata",
+   type : "object",
+   
+   description : "A Metadata entry is a key-value pair, with provenance information, associated with a particular request. The metadata entry's key may be additionally parsed for identification of the value within a bulk-request submission.",
+   
+   properties : { 
+   
+     metadata_key : { 
+        type : "string",
+        description : 
+            "The string by which this metadata entry may be identified.",
+     },
+     
+     metadata_value : { 
+        type : "string",
+        description : "The value of this metadata entry.",
+     },
+     
+     created_on : { 
+        type : "string",
+        description : "A string field, of format yyyy-MM-dd, describing when this metadata value was first created and attached to the corresponding request.",
+     },
+     
+     created_by : { 
+         type : "Link",
+         description : "A link to the URI identifying the user who created this metadata entry.",
+     },
    }
  }
